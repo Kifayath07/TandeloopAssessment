e93e4269554d35dbc62c8d32d9710f7b6ef96526
Mohammed Arbaaz <kifayathmuhammad@gmail.com>
2021-09-16 22:07:49 +0530

    First Commit

--- TandeloopAssessment/src/com/tandeloopAssessment/test/Program_2.java
+++ TandeloopAssessment/src/com/tandeloopAssessment/test/Program_2.java
@@ -3,30 +3,19 @@ package com.tandeloopAssessment.test;
 import java.util.Scanner;
 
 public class Program_2 {
-
-	static int sum(int n) {
-		int num = 0;
-		while(n>0 || num>9)
-		{
-			if(n == 0) {
-				n = num;
-				num = 0;
-				
-			}
-			num += n%10;
-			n/= 10;
-		}
-		return num;
-	}
 		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int limit ,i;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of your choice");
-		int n = sc.nextInt();
-	
-		System.out.println(sum(n));
-
+		System.out.print("Enter the InputLimit : ");
+		limit = sc.nextInt();
+		for(i =1 ; i<= limit ; i++) {
+			if(i%2 != 0) {
+				System.out.println(i);
+				
+			}
+		}
 	}
 
 }
