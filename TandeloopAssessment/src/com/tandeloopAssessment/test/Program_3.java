2345dc6f259f9720f2043c051e65e4e6603d380b
Mohammed Arbaaz <kifayathmuhammad@gmail.com>
2021-09-17 10:17:18 +0530

    First Commit

--- TandeloopAssessment/src/com/tandeloopAssessment/test/Program_3.java
+++ TandeloopAssessment/src/com/tandeloopAssessment/test/Program_3.java
@@ -1,10 +1,23 @@
 package com.tandeloopAssessment.test;
 
-public class Program_3 {
+import java.util.Scanner;
 
+public class Program_3 {
+	
+	static int digit(int n) {
+		if(n == 1) {
+			return 1;
+		}
+		String s = String.valueOf(n);
+		return s.length() + digit(n-1);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n = sc.nextInt();
+		System.out.println(digit(n));
 	}
 
 }
